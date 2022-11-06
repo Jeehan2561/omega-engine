d65459a59c3ca240cfb6ed4eafa46fb578f3f5da
Jeehan2561 <88998566+Jeehan2561@users.noreply.github.com>
2022-11-06 18:31:16 +0700

    v1.7.0.3

--- js/mods/functions-locked-button.js
+++ js/mods/functions-locked-button.js
@@ -7,9 +7,4 @@ Vue.component("functions-locked-button", {
     template: `<button @click="$emit('click')" :disabled="Decimal(totalFunctions()).lt(functions)">
     <span v-if="Decimal(totalFunctions()).lt(functions)">Reach {{functions}}<span class="aleph">ƒP</span></span>
     <span v-else><slot></slot></span>
-</button>`
-<<<<<<< HEAD
-})
-=======
-})
->>>>>>> 213df0d5ad581f9042197c68cb381c7a42db1bc4
+</button>`})
